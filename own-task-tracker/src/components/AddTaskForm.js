98a07dbfe4a2364433556ed179b05a165123d139
jordydp <jdepril@outlook.com>
2022-04-13 11:51:11 +0200

    Import React Icons + Setup Header.js & AddTaskForm

---
+++ own-task-tracker/src/components/AddTaskForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddTaskForm = () => {
+  return (
+    <form>
+        AddTaskForm
+    </form>
+  )
+}
+
+export default AddTaskForm
---
+++ own-task-tracker/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {BiAddToQueue} from 'react-icons/bi'
+
+const Header = () => {
+return (
+    <header>
+        <div>
+        <h1>Click + To Add Task
+            <BiAddToQueue />
+        </h1>
+        </div>
+    </header>
+)
+}
+
+export default Header
